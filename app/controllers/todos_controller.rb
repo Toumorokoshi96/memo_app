e1711a99741e9f683a20816f169f2bc4e71e0055
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-09-27 10:36:31 +0900

    pre training has been finished

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,24 @@
 class TodosController < ApplicationController
+  def index
+    @todo = Todo.new
+    @todos = Todo.order('created_at ASC')
+  end
+
+  # def create
+  #   Todo.create(todo_params)
+  #   redirect_to :root
+  # end
+
+  def create
+    @todo = Todo.create(todo_params)
+    respond_to do |format|
+      format.html { redirect_to :root }
+      # format.json { render json: @todo}
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content)
+  end
 end
